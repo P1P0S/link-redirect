173b5c1caf855ee3e209b2889051a1a79e5946c6
Marnei Machado <marnei.machado@gmail.com>
2022-06-29 17:25:37 -0300

    add: mongoose schema
    create default schema and save fictitious value on db

--- app.js
+++ app.js
@@ -3,6 +3,33 @@ const app = express();
 const PORT = 3000;
 const mongoose = require("mongoose");
 
+const linkSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  url: { type: String, required: true },
+  clicks: { type: Number, default: 0 },
+});
+
+//(L)ink = model/collection, (l)ink = document
+const Link = mongoose.model("Link", linkSchema);
+
+//fictitious values
+let link = new Link({
+  title: "Pipos",
+  description: "Link para o youtube",
+  url: "https://youtube.com",
+});
+
+//insert values on db
+link
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 mongoose.connect("mongodb://localhost/links", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
