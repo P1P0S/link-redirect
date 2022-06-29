d26410e0d53c09e6e7cf67ac55348de1eaecb491
Marnei Machado <marnei.machado@gmail.com>
2022-06-29 15:48:51 -0300

    add: mongoose db localhost

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/links", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("Houve algum erro");
+});
+
+db.once("open", () => {
+  console.log("Banco de dados carregado!");
+});
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
