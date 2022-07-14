c25b040d5623761381f896611c3182d3a4812006
Marnei Machado <marnei.machado@gmail.com>
2022-07-14 20:03:36 -0300

    add new route

--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -6,4 +6,10 @@ router.get("/:title", linkController.redirect);
 
 router.get("/", (req, res) => res.send("Hello World!"));
 
+router.post(
+  "/",
+  express.urlencoded({ extended: true }),
+  linkController.addLink
+);
+
 module.exports = router;
