ab263a62e7109b72716028a8d79930ad16699c6a
Marnei Machado <marnei.machado@gmail.com>
2022-07-26 14:06:48 -0300

    add: edit link page

--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -6,9 +6,8 @@ const methodOverride = require("method-override");
 router.use(methodOverride("_method"));
 
 router.get("/", linkController.allLinks);
-
 router.get("/:title", linkController.redirect);
-
+router.get("/edit/:id", linkController.loadLink);
 router.get("/add", (req, res) =>
   res.render("index", { error: false, body: {} })
 );
@@ -18,9 +17,13 @@ router.post(
   express.urlencoded({ extended: true }),
   linkController.addLink
 );
+router.post(
+  "/:edit/:id",
+  express.urlencoded({ extended: true }),
+  linkController.editLink
+);
 
 router.delete("/:id", linkController.deleteLink);
-
 router.delete(
   "/",
   express.urlencoded({ extended: true }),
